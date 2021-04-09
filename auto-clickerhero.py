a2c4a8ff0fc506ef404c85744942cd5aa0031f26
Paul Dudley <p4ttch@gmail.com>
2021-04-09 23:56:02 +0200

    added threading to the clicker for faster clicks

--- auto-clickerhero.py
+++ auto-clickerhero.py
@@ -1,6 +1,7 @@
 # library comes from : https://pyautogui.readthedocs.io/en/latest/quickstart.html
 import pyautogui as pg
 import time
+import threading
 
 #Objective:
 # Make a Clicker Heros bot to progress in levels
@@ -18,7 +19,7 @@ def killmonsters(clickCounter):
     global gamePhase
     #Move mouse to hover over monsters
     pg.moveTo(999, 429, duration=0.1)
-    while clicks < 20 :
+    while clicks < 25 :
         pg.click()
         pg.click()
         clicks += 1
@@ -56,18 +57,39 @@ def resetLoop():
         # Prints the current time with a five second difference
         time.sleep(1) # important pause to regain control of the mouse haha.
 
+
+def phase_manager():        
+    while gamePhase < 4:
+        if (gamePhase == 0):
+            killmonsters(clicks)
+        # if (gamePhase == 1):
+        #     nextlevel()
+        # if (gamePhase == 2):
+        #     upgrade()
+        # if (gamePhase == 3):
+        #    resetLoop()
+        if (gamePhase == 1):
+            resetLoop()
+# killmonsters(clicks)
+
 print("The script will start in 5 seconds. Please get your clicker hero window ready.")
 for count in range(5):
         print(time.ctime())
         time.sleep(1)
-while gamePhase < 4:
-    if (gamePhase == 0):
-        killmonsters(clicks)
-    if (gamePhase == 1):
-        nextlevel()
-    if (gamePhase == 2):
-        upgrade()
-    if (gamePhase == 3):
-       resetLoop()
-# killmonsters(clicks)
+
+# phase_manager()
+
+# I dont think threading would work well for the normal auto clicker as far as going up in levels just yet, but maybe if i shuffled some things around. But it does increase the speed of clicks greatly hahaha
+threads = []
+
+for i in range(5):
+    t = threading.Thread(target=phase_manager)
+    t.daemon = True
+    threads.append(t)
+
+for i in range(5):
+    threads[i].start()
+
+for i in range(5):
+    threads[i].join()
     
